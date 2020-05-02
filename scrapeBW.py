95e4eead50b9a51c598d687472e9fd02abb77b21
Dante Gil-Marin <drgilmarin@icloud.com>
2020-05-02 00:37:40 -0500

    help lol

--- scrapeBW.py
+++ scrapeBW.py
@@ -15,7 +15,9 @@ def scrapeBW():
     wait = WebDriverWait(driver, 4)
     driver.get('https://sports.betway.com/en/sports/sct/esports/cs-go')
 
-    Time.sleep(3)
+    # driver.find_element_by_xpath('//*[@class="arrowIcon icon-arrow-right"]').click() <--- this one right here officer
+
+    Time.sleep(5)
     soup = BeautifulSoup(driver.page_source,  features="html.parser")
 
     days = soup.find_all("div", class_="collapsablePanel alternativeHeaderBackground")
@@ -26,7 +28,9 @@ def scrapeBW():
             title = dd.find("div", class_="titleText")
             matches = dd.find_all("div", class_="oneLineEventItem")
             for match in matches:
-                matchtime = match.find("div", class_="oneLineDateTime").text
+                time_text = match.find("div", class_="oneLineDateTime").text
+                time = datetime.strptime(time_text, '%H:%M').time()
+                matchtime = datetime.combine(date.today(), time)
                 team1name = match.find("span", class_="teamNameFirstPart teamNameHomeTextFirstPart")
                 if team1name == None:
                     team1name = match.find("span", class_="teamNameFirstPart teamNameHomeTextFirstPart smallFont")
@@ -39,24 +43,5 @@ def scrapeBW():
                 team1odds = odds[0].text
                 team2odds = odds[1].text
                 print(f"Time: {matchtime} Team 1: {team1name} odds: {team1odds} Team 2: {team2name} odds: {team2odds}")
-
-    # for match in matches:
-    #     time_set = match.find_all("div", class_="matchDateTime__date___2Hw-c")
-    #     time_to_match = [(i.contents[0].text, i.contents[1]) for i in time_set]
-    #     year = str(date.today().year)
-    #     time_string = str(time_to_match[0][1]) + \
-    #         ' '+year+' ' + str(time_to_match[0][0])
-    #     matchtime = datetime.strptime(time_string, '%b %d %Y %H:%M')
-    #     teams = match.find_all(
-    #         "div", class_="__app-LogoTitle-name LogoTitle__name___2LTlu")
-    #     team1name = teams[0].get_text()
-    #     team2name = teams[1].get_text()
-    #     odds = match.find_all("div", class_="odd__ellipsis___3b4Yk")
-    #     if len(odds) == 0:
-    #         continue
-    #     team1odds = odds[0].get_text()
-    #     team2odds = odds[1].get_text()
-    #     print(
-    #         f"Time: {matchtime} Team 1: {team1name} odds: {team1odds} Team 2: {team2name} odds: {team2odds}")
 
 scrapeBW()
